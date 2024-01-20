bbd657a6fb8199b2bb482881066dc29fc8377bf3
fatimasgit <fatimamedova@inbox.ru>
2024-01-20 02:34:25 -0800

    sorting an array by using the Quick sorting"

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,5 +1,19 @@
 #include "sort.h"
 /**
+ * swap_ints - Swaps two integers in an array.
+ * @a: The first integer to swap.
+ * @b: The second integer to swap.
+ */
+void swap_ints(int *a, int *b)
+{
+	int tmp;
+
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
+
+/**
  * l_partition - Order a subset of an array of integers according to
  *  the lomuto partition scheme.
  * @array: The array of integers.
