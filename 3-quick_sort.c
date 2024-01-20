04e6e0f3ebb6b8a766c651f6a0e6a1c5813fe69e
fatimasgit <fatimamedova@inbox.ru>
2024-01-20 02:25:20 -0800

    sorting an array by using the Quick sorting"

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,12 +1,30 @@
 #include "sort.h"
 
+
+void sort(int *array, size_t size, int left, int right)
+{
+	int part;
+
+	if (right - left > 0)
+	{
+		part = partition(array, size, left, right);
+		sort(array, size, left, part - 1);
+		sort(array, size, part + 1, right);
+	}
+}
+
 /**
- * quick_sort - sorts an array of integers using the Quick
- * sort algorithm in ascending order
- * @array: array to sort
- * @size: array size
+ * quick_sort - Sort an array of integers in ascending
+ * order using the quicksort algorithm.
+ * @array: An array of integers.
+ * @size: The size of the array.
+ * Description: Uses the  partition scheme.
  */
+
 void quick_sort(int *array, size_t size)
 {
-	qs(array, 0, size - 1, size);
+	if (array == NULL || size < 2)
+		return;
+
+	sort(array, size, 0, size - 1);
 }
